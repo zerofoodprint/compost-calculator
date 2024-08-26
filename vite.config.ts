9b40453e73249e06e7d71011db909b5c3c500a8a
Chris Styles <christopherstyles@users.noreply.github.com>
2024-08-26 15:21:09 -0700

    Add embed page

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,18 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import react from "@vitejs/plugin-react";
+import { resolve } from "node:path";
+import { defineConfig } from "vite";
 
 // https://vitejs.dev/config/
+declare const __dirname: string;
+
 export default defineConfig({
+  build: {
+    rollupOptions: {
+      input: {
+        main: resolve(__dirname, "index.html"),
+        embed: resolve(__dirname, "embed.html"),
+      },
+    },
+  },
   plugins: [react()],
-})
+});
